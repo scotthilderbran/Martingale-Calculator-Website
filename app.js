84e017a1fbe47ca50c9b65357b35cd4105596ad4
Scott Hilderbran <scott@Scotts-MacBook-Pro.local>
2019-06-10 17:04:25 -0400

    Added bootstrap grid layout

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 console.dir(document);
-
+document.getElementById("submitme").addEventListener("click", function(){
+    let betAmount = document.getElementById("betamount").value;
+    let bankRoll = document.getElementById("bankroll").value;
+    console.log(betAmount);
+    console.log(bankRoll);
+    let count = 0;
+    while (bankRoll > 0){
+        bankRoll -= betAmount;
+        betAmount *= 2;
+        count ++;
+        console.log(bankRoll);
+        console.log(betAmount);
+        console.log(count);
+    }
+    document.getElementById("test").innerHTML = `You were able to take: ${count} consecutive losses.`;
+  });
